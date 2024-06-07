d11975b72b5ff08d655871f6479901c5e003a78e
Mohd Ali <mohdali770409@gmail.com>
2024-06-08 00:16:04 +0530

    home page, property page frontend completed with loading and not-found page

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import React from "react";
 // below @ means from root directory that's how we access
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 import "@/assets/styles/globals.css";
+
 export const meta = {
   title: "Pulse Rental Website",
   description: "find your dream rental property",
@@ -10,7 +13,10 @@ const MainLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
+        <Navbar/>
         <div>{children}</div>
+        <Footer/>
+
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,9 +1,18 @@
+"use client";
+import Hero from "@/components/Hero";
+import HomeProperties from "@/components/HomeProperties";
+import InfoBoxes from "@/components/InfoBoxes";
 
-
+import { useRouter } from "next/navigation";
 const HomePage = () => {
+  const router = useRouter();
   return (
-    <div>HomePage</div>
-  )
-}
+    <>
+      <Hero />
+      <InfoBoxes />
+      <HomeProperties />
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
